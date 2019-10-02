1d98b07c6263f9cbc1716becc4bdb34cb7545f55
tpombeiro87 <tpombeiro87@gmail.com>
2019-10-02 18:29:03 +0200

    feat: using sendgrid intead of mailgun

--- src/send-email.js
+++ src/send-email.js
@@ -1,4 +1,4 @@
-const mailgun = require('mailgun-js')
+const sgMail = require('@sendgrid/mail')
 
 const htmlGenerator = require('./html-generator')
 const writeContactOnDb = require('./write-db')
@@ -10,32 +10,28 @@ const sendEmail = (req, res) => {
     return res.status(400).send({ error: 'no JSON object in the request' })
   }
 
-  const data = {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
     from: 'Site Goiaba Clothes <no-reply@goiabaclothes.pt>',
     to: `${process.env.TECNICAL_EMAIL}, ${process.env.SALES_EMAIL}`,
     subject: 'Goiaba Clothes Site - Pedido de compra',
     html: htmlGenerator(req.body || {}),
   }
-  const mailgunInstance = mailgun({
-    apiKey: process.env.MAILGUN_API_KEY,
-    domain: process.env.MAILGUN_DOMAIN,
+  sgMail.send(msg, false, (err, emailServiceDetails) => {
+    if (err) {
+      console.log(err)
+      return res.status(500).send({ err: err })
+    }
+    return writeContactOnDb(req.body)
+      .then(({ dbsaved, dbError }) =>
+        res.status(200).send({
+          msg: `Email sent!`,
+          emailServiceDetails,
+          dbsaved,
+          dbError,
+        })
+      )
   })
-  mailgunInstance.messages()
-    .send(data, (err, emailServiceDetails) => {
-      if (err) {
-        return res.status(500).send({ err: err })
-      }
-      console.log(emailServiceDetails)
-      return writeContactOnDb(req.body)
-        .then(({ dbsaved, dbError }) =>
-          res.status(200).send({
-            msg: `Email sent!`,
-            emailServiceDetails,
-            dbsaved,
-            dbError,
-          })
-        )
-    })
 }
 
 module.exports = sendEmail
